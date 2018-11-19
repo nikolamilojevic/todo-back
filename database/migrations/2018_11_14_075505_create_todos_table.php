641c4901a0d23caab96da1f469e9cce14472e2a7
Nikola <nikola.milojevic@vivifyideas.com>
2018-11-19 20:03:33 +0100

    handling http requests

--- database/migrations/2018_11_14_075505_create_todos_table.php
+++ database/migrations/2018_11_14_075505_create_todos_table.php
@@ -16,7 +16,7 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->increments('id');
             $table->text('description');
-            $table->string('priority');
+            $table->integer('priority');
             $table->string('status');
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::resource('todos', 'TodosController');
+
 Route::group([
 
     'middleware' => 'api',
